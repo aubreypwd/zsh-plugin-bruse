63c520b7e542b5ef95eb04ddeef268aa626cb73a
Aubrey Portwood <code@aubreypwd.com>
2020-10-02 11:30:24 -0600

    Add what I have so far.

--- zsh-plugin-bruse.plugin.zsh
+++ zsh-plugin-bruse.plugin.zsh
@@ -1,19 +1,39 @@
 #!/bin/zsh
 
 if [[ $(command -v antigen) ]]; then
-	antigen bundle aubreypwd/zsh-plugin-require@1.0.1
-	antigen apply
+	# antigen bundle aubreypwd/zsh-plugin-require@1.0.1
+	# antigen apply
 
-	require "{BREWCOMMAND}" "brew reinstall {BREWCOMMAND}" "brew"
+	# require "brew" "" "brew"
 fi
 
-if ! [[ -x $(command -v {BREWCOMMAND}) ]]; then
-	echo "Please install {BREWCOMMAND} to use {NAME}."
-	return;
-fi
+# bruse php 7.2
 
-###
- #
- #
- # @since 1.0.0
- ##
+function bruse {
+	if ! [[ -x $(command -v brew) ]]; then >&2 echo "This requires that you use Homebrew (https://brew.sh), we couldn't find the 'brew' command." && return; fi
+	if ! [[ -x $(command -v xargs) ]]; then >&2 echo "This requires xargs, please install." && return; fi
+
+	local package="$1"
+	local version="$2"
+
+	if [[ -z $( brew ls --versions "$package@$version" ) ]]; then
+		echo "$package@$version not installed, trying to install..."
+		brew install "$package@$version"
+	fi
+
+	if [[ -z $( brew ls --versions "$package@$version" ) ]]; then
+		echo "Unable to install or use $package@$version."
+		return
+	fi
+
+	if [[ $( brew link "$package@$version" --force --overwrite ) == *"relink:"* ]]; then # Try and link the new version, unless it tells us we need to relink...
+		local relink=$( echo "$( brew link $package@$version --force --overwrite )" | grep -Eo '  (.*)' ) # Get the relink command that brew is giving us.
+		local relink=$( echo "$relink" | xargs ) # Trim the command.
+		local relink="$relink --overwrite" # Make sure we overwrite anything.
+
+		if [[ $relink == *"brew unlink"* ]]; then # Confirm we have an unlink command.
+			echo "Issuing: $relink"
+			eval "$relink"
+		fi
+	fi
+}
